cee3c8442049666cd6e399054305d964e85cb091
Damien Morcellet <dmorcellet@free.fr>
2011-02-14 16:30:31 +0000

    delta4j/common:     * Path class improvements

--- src/main/java/delta/common/utils/files/Path.java
+++ src/main/java/delta/common/utils/files/Path.java
@@ -2,61 +2,110 @@ package delta.common.utils.files;
 
 import java.io.File;
 
+import delta.common.utils.text.StringSplitter;
+
+/**
+ * Represents a path.
+ * @author DAM
+ */
 public class Path
 {
   private String[] _paths;
   private String _path;
+  private char _separator;
 
+  /**
+   * Default constructor. Builds a path with no entry.
+   */
   public Path()
   {
-    _paths=new String[]{};
-    _path="";
+    this(new String[]{},"",File.separatorChar);
   }
 
-  public Path(String path)
+  /**
+   * Simple constructor. Builds a one entry path.
+   * @param path Entry of this path.
+   * @param separator Separator to use.
+   */
+  public Path(String path, char separator)
   {
-    _paths=new String[]{path};
-    _path=path;
+    this(StringSplitter.split(path,separator),path,separator);
   }
 
-  private Path(String[] paths, String path)
+  private Path(String[] paths, String path, char separator)
   {
     _paths=paths;
     _path=path;
+    _separator=separator;
   }
 
-  public Path buildChildPath(String childPath)
+  /**
+   * Build a new path using this path and a child entry.
+   * @param childEntry Entry to add to this path.
+   * @return the newly built path.
+   */
+  public Path buildChildPath(String childEntry)
+  {
+    int index=childEntry.indexOf(_separator);
+    if (index!=-1)
+    {
+      String entry=childEntry.substring(0,index);
+      Path tmp=unsafeBuildChildPath(entry);
+      return tmp.buildChildPath(childEntry.substring(index+1));
+    }
+    return unsafeBuildChildPath(childEntry);
+  }
+  
+  private Path unsafeBuildChildPath(String childEntry)
   {
     String[] paths=new String[_paths.length+1];
     System.arraycopy(_paths,0,paths,0,_paths.length);
-    paths[_paths.length]=childPath;
+    paths[_paths.length]=childEntry;
     String path;
     if (_path.length()==0)
     {
-      path=childPath;
+      path=childEntry;
     }
     else
     {
-      path=_path+File.separator+childPath;
+      path=_path+_separator+childEntry;
     }
-    return new Path(paths,path);
+    return new Path(paths,path,_separator);
   }
 
+  /**
+   * Get this path as a string, using the system's file separator as the
+   * separator between path entries.
+   * @return A path string.
+   */
   public String getPath()
   {
     return _path;
   }
 
+  /**
+   * Get the number of entries in this path.
+   * @return A positive number.
+   */
   public int getLevel()
   {
-    if (_paths==null) return 0;
-    return _paths.length;
+    return (_paths!=null)?_paths.length:0;
   }
 
+  /**
+   * Get the path entry at the specified level
+   * (starting at 0, in ascending order from parent to child).
+   * @param level Level to get.
+   * @return A path entry or <code>null</code> if not found.
+   */
   public String getPath(int level)
   {
     if (_paths==null) return null;
-    return _paths[level];
+    if ((level>=0) && (level<_paths.length))
+    {
+      return _paths[level]; 
+    }
+    return null;
   }
 
   @Override
