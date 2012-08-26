f772e975935e52e37091339c3186947f1e55553b
Damien Morcellet <dmorcellet@free.fr>
2012-08-26 12:42:46 +0000

    delta4j/common:     * test class 'MainTestPDAG' now uses class ExternalCommand instead of ProcessManager

--- src/test/java/delta/common/utils/system/externalCommands/MainTestPDAG.java
+++ src/test/java/delta/common/utils/system/externalCommands/MainTestPDAG.java
@@ -1,29 +1,29 @@
-package delta.common.utils.processManagement;
+package delta.common.utils.system.externalCommands;
 
 import delta.common.utils.misc.SleepManager;
 
 public class MainTestPDAG
 {
-  public static int executeCommandAsynchronous(String command_p)
+  public static ExecutionStatus executeCommandAsynchronous(String command_p)
   {
-    ProcessManager pm=new ProcessManager(command_p);
+    ExternalCommand pm=new ExternalCommand(command_p);
     pm.executeAsynchronously();
     System.out.println("Start date : "+pm.getStartDate());
     SleepManager.sleep(5000);
     pm.stopProcess();
     System.out.println("End date : "+pm.getEndDate());
-    long duration_l=pm.getEndDate()-pm.getStartDate();
+    long duration_l=pm.getEndDate().longValue()-pm.getStartDate().longValue();
     System.out.println("Duration (ms) = "+duration_l);
     return pm.getExecutionStatus();
   }
 
-  public static int executeCommandSynchronous(String command_p)
+  public static Integer executeCommandSynchronous(String command_p)
   {
-    ProcessManager pm=new ProcessManager(command_p);
-    int return_l=pm.executeSynchronously();
+    ExternalCommand pm=new ExternalCommand(command_p);
+    Integer return_l=pm.executeSynchronously();
     System.out.println("Start date : "+pm.getStartDate());
     System.out.println("End date : "+pm.getEndDate());
-    long duration_l=pm.getEndDate()-pm.getStartDate();
+    long duration_l=pm.getEndDate().longValue()-pm.getStartDate().longValue();
     System.out.println("Duration (ms) = "+duration_l);
     return return_l;
   }
