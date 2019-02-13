baef7a7334344e0d1cf13f73c1db78641c871290
Damien Morcellet <dmorcellet@free.fr>
2019-02-13 22:56:52 +0100

    Improved the CompoundFilter

--- src/main/java/delta/common/utils/collections/filters/CompoundFilter.java
+++ src/main/java/delta/common/utils/collections/filters/CompoundFilter.java
@@ -31,6 +31,52 @@ public class CompoundFilter<T> implements Filter<T>
     _filters.addAll(filters);
   }
 
+  /**
+   * Constructor.
+   * @param operator Logical operator.
+   * @param filters Sub-filters.
+   */
+  @SafeVarargs
+  public CompoundFilter(Operator operator, Filter<T>... filters)
+  {
+    _operator=operator;
+    _filters=new ArrayList<Filter<T>>();
+    for(Filter<T> filter : filters)
+    {
+      _filters.add(filter);
+    }
+  }
+
+  /**
+   * Add a filter.
+   * @param filter Filter to add.
+   */
+  public void addFilter(Filter<T> filter)
+  {
+    if (!_filters.contains(filter))
+    {
+      _filters.add(filter);
+    }
+  }
+
+  /**
+   * Get all managed filters.
+   * @return a list of all managed filters.
+   */
+  public List<Filter<T>> getFilters()
+  {
+    return new ArrayList<Filter<T>>(_filters);
+  }
+
+  /**
+   * Get the managed operator.
+   * @return the managed operator.
+   */
+  public Operator getOperator()
+  {
+    return _operator;
+  }
+
   public boolean accept(T item)
   {
     if (_operator==Operator.AND)
