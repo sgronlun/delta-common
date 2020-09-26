119efb6aa5e1b34f24bf764101b4bb1386014fc1
Damien Morcellet <dmorcellet@free.fr>
2020-09-26 11:33:12 +0200

    Added Identifiable and IdentifiableComparator

---
+++ src/main/java/delta/common/utils/id/Identifiable.java
@@ -0,0 +1,14 @@
+package delta.common.utils.id;
+
+/**
+ * Identifiable.
+ * @author DAM
+ */
+public interface Identifiable
+{
+  /**
+   * Get the identifier of this object.
+   * @return an object identifier.
+   */
+  int getIdentifier();
+}
