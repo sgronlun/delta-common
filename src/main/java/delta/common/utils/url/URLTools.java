816fabcfe678e115eb196e3f25d5eee9cc77528c
Damien Morcellet <dmorcellet@free.fr>
2021-01-31 12:15:34 +0100

    Improvements in URLTools to bette support classloaders

--- src/main/java/delta/common/utils/url/URLTools.java
+++ src/main/java/delta/common/utils/url/URLTools.java
@@ -11,20 +11,6 @@ import java.net.URL;
 public abstract class URLTools
 {
   /**
-   * Build an URL from a resource name and a package name.
-   * @param name Resource name.
-   * @param packageName Name of package to use.
-   * @return Found URL or <code>null</code> if not found.
-   */
-  public static URL getFromClassPath(String name, String packageName)
-  {
-    String resourceToFind=packageName;
-    resourceToFind=resourceToFind.replace('.','/');
-    resourceToFind=resourceToFind+"/"+name;
-    return getFromClassPath(resourceToFind);
-  }
-
-  /**
    * Build an URL from a resource name and an object.
    * Resource is searched using the package of object's class as root.
    * @param name Resource name.
@@ -33,8 +19,8 @@ public abstract class URLTools
    */
   public static URL getFromClassPath(String name, Object instance)
   {
-    Package p=instance.getClass().getPackage();
-    return getFromClassPath(name,p);
+    Class<?> clazz=instance.getClass();
+    return getFromClassPath(name,clazz);
   }
 
   /**
@@ -42,20 +28,64 @@ public abstract class URLTools
    * @param name Resource name.
    * @param startPoint Package that serves as a start point.
    * @return Found URL or <code>null</code> if not found.
+   * @deprecated Use version with class instead.
    */
+  @Deprecated
   public static URL getFromClassPath(String name, Package startPoint)
+  {
+    return getFromClassPath(name,startPoint,null);
+  }
+
+  /**
+   * Build an URL from a resource name and a class start point.
+   * @param name Resource name.
+   * @param clazz Class that serves as a start point.
+   * @return Found URL or <code>null</code> if not found.
+   */
+  public static URL getFromClassPath(String name, Class<?> clazz)
+  {
+    Package p=clazz.getPackage();
+    ClassLoader classloader=clazz.getClassLoader();
+    return getFromClassPath(name,p,classloader);
+  }
+
+  /**
+   * Build an URL from a resource name and a package start point.
+   * @param name Resource name.
+   * @param startPoint Package that serves as a start point.
+   * @param classloader Class loader to use.
+   * @return Found URL or <code>null</code> if not found.
+   */
+  private static URL getFromClassPath(String name, Package startPoint, ClassLoader classloader)
   {
     String resourceToFind=startPoint.getName();
     resourceToFind=resourceToFind.replace('.','/');
     resourceToFind=resourceToFind+"/"+name;
+    if (classloader!=null)
+    {
+      return getFromClassPath(resourceToFind,classloader);
+    }
     return getFromClassPath(resourceToFind);
   }
 
   /**
    * Look for a resource in the CLASSPATH and return it as an URL.
    * @param name Resource to find.
+   * @param classloader Class loader to use.
    * @return Found URL or <code>null</code> if not found.
    */
+  public static URL getFromClassPath(String name, ClassLoader classloader)
+  {
+    return classloader.getResource(name);
+  }
+
+  /**
+   * Look for a resource in the CLASSPATH and return it as an URL.
+   * @param name Resource to find.
+   * @return Found URL or <code>null</code> if not found.
+   * @deprecated Use version with class or instance: it is classloader aware.
+   */
+  @Deprecated
   public static URL getFromClassPath(String name)
   {
     ClassLoader loader=URLTools.class.getClassLoader();
