60c50fad0c3f70f9bf5758e60ad43e39bc0ff154
Damien Morcellet <dmorcellet@free.fr>
2023-05-26 18:00:36 +0200

    Definition of a context (issue LotroCompanion/lotro-companion#481)

---
+++ src/main/java/delta/common/utils/context/Context.java
@@ -0,0 +1,35 @@
+package delta.common.utils.context;
+
+/**
+ * Context.
+ * @author DAM
+ */
+public interface Context
+{
+  /**
+   * Get the parent context.
+   * @return A context or <code>null</code> if no parent.
+   */
+  Context getParentContext();
+
+  /**
+   * Get the value of a given key.
+   * <p>Note: this will not use the parent context if any.
+   * @param <T> Type of expected value.
+   * @param key Key to use.
+   * @param clazz Typed of data to get.
+   * @return A value or <code>null</code> if not found.
+   */
+  <T> T getValue(String key, Class<T> clazz);
+
+  /**
+   * Remove a value.
+   * @param key Key to remove.
+   */
+  void removeValue(String key);
+
+  /**
+   * Clear all data in this context.
+   */
+  void clear();
+}
